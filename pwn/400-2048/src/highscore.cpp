e8318b399270580a0560f2e8c540a4234478b6e9
tophertimzen <c.timzen@gmail.com>
2017-09-16 11:29:48 -0700

    Changes to pwn400-2048
    Fixed spacing in patch file
    Fixed spacing in highscore.cpp
    Changes to Dockerfile

--- pwn/400-2048/src/highscore.cpp
+++ pwn/400-2048/src/highscore.cpp
@@ -4,7 +4,7 @@
 bool eraseHighScore()
 {
     size_t res = remove(fhighscore);
-    return (res == 1); 
+    return (res == 1);
 }
 
 bool setHighScore(int maxTile, int highscore)
@@ -12,30 +12,30 @@ bool setHighScore(int maxTile, int highscore)
     if (maxTile > highscore)
     {
         FILE* f = fopen(fhighscore, "w");
-        char buffer[2048] = {0};    
+        char buffer[2048] = {0};
         int key;
         int count = 0;
 
         if (f == NULL) return false;
-        size_t res = fwrite(&maxTile, sizeof(maxTile), 1, f); 
+        size_t res = fwrite(&maxTile, sizeof(maxTile), 1, f);
 
         printf("\n\nWhat is your name?\n");
-        do 
+        do
         {
             key=getkey();
             buffer[count] = key;
             count ++;
         } while (key != '\n' && count < 2048-1);
- 
+
         buffer[count] = '\0';
-        
+
         printf("New highscore!");
         printf(" %d belongs to - ", maxTile);
         printf(buffer);
         res = fwrite(buffer, sizeof(buffer), 1, f);
             key=getkey();
         fclose(f);
-        return (res == 1); 
+        return (res == 1);
     }
     return false;
 }
@@ -45,10 +45,10 @@ int loadHighScore(char **playerName)
     int highscore;
     FILE* f = fopen(fhighscore, "r");
     if (f == NULL) return false;
-    size_t res = fread(&highscore, sizeof(highscore), 1, f); 
+    size_t res = fread(&highscore, sizeof(highscore), 1, f);
     if (!res)
         return 0;
-    
+
    *playerName = (char *) malloc(100);
 
     if (*playerName == NULL)
